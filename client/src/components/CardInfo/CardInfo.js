97fda94d7cdd5487987773a591309c855d84c436
Арман Гаспарян <buscus111@mail.ru>
2021-03-27 21:06:50 +0300

    added update button to card-info page

--- client/src/components/CardInfo/CardInfo.js
+++ client/src/components/CardInfo/CardInfo.js
@@ -1,7 +1,9 @@
 import './CardInfo.css'
-import { Link } from 'react-router-dom'
-import { useSelector } from 'react-redux'
+import { Link, useParams } from 'react-router-dom'
+import { useSelector, useDispatch } from 'react-redux'
 import styled from 'styled-components'
+import getCardInfo from '../../redux/thunks/getCardInfo'
+import getAllPockemonsThunk from '../../redux/thunks/getAllPockemons'
 
 const OneCardWrapper = styled.div`{
   width: 40vw;
@@ -13,13 +15,20 @@ const OneCardWrapper = styled.div`{
 }`
 
 function CardInfo() {
+  const dispatch = useDispatch()
   const currentCard = useSelector(state => state.currentCard);
+  const { id } = useParams()
+  const handlerUpdate = () => {
+    dispatch(getAllPockemonsThunk())
+    dispatch(getCardInfo(id))
+  };
   //можно было выводить инфу всю вместо карточки, но на карточке и так все расписанно. 
   return (
     <OneCardWrapper>
       <img className="current-card-img" src={currentCard?.imageUrlHiRes} alt=""/>
+      <button onClick={handlerUpdate}  type="button" className="btn btn-light mt-1">Update</button>
       <Link to="/cards">
-        <button  type="button" className="btn btn-light mt-5">Back</button>
+        <button  type="button" className="btn btn-light mt-1">Back</button>
       </Link>
     </OneCardWrapper>
   )
